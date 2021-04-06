dfdec519aef65362745b564dc00d3518c8eaf576
WellingtonFreitas <wellington.m.defreitas@gmail.com>
2021-04-05 21:06:38 -0300

    configuraçãi dapper e documentação swagger

--- DevCars.API/Controllers/CarsController.cs
+++ DevCars.API/Controllers/CarsController.cs
@@ -1,8 +1,11 @@
-﻿using DevCars.API.Entities;
+﻿using Dapper;
+using DevCars.API.Entities;
 using DevCars.API.InputModels;
 using DevCars.API.Persistence;
 using DevCars.API.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,19 +17,52 @@ namespace DevCars.API.Controllers
     public class CarsController : ControllerBase
     {
         private readonly DevCarsDbContext _dbContext;
-        public CarsController(DevCarsDbContext dbContext)
+        private readonly string _connectionString;
+
+        public CarsController(DevCarsDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            //usando dapper
+            _connectionString = configuration.GetConnectionString("DevCarsCs");
         }
         // GET api/cars
+        /// <summary>
+        /// Cadastrar um Carro
+        /// </summary>
+        /// <remarks>
+        /// Requisição de exemplo:
+        /// {
+        ///     "brand" : "Honda",
+        ///     "model" : "Civic,
+        ///     "vinCode" : "abc123",
+        ///     "year" : 2021,
+        ///     "color" : "Cinza",
+        ///     "price": 110000,
+        ///     "productionDate": "2021-04-05"
+        /// }
+        /// </remarks>
+        /// <param name="model"> Dados de um novo carro</param>
+        /// <returns code="200">Objeto criado com suceso.</returns>
+        ///  <returns code="500">Erro ao criar um objeto.</returns>
         [HttpGet]
         public IActionResult Get()
         {
             try
             {
+                //com entity
                 var cars = _dbContext.Cars;
                 var carsViewModel = cars.Where(c => c.Status == CarsStatusEnum.Available).Select(c => new CarItemViewModel(c.Id, c.Brand, c.Model, c.Price)).ToList();
                 return Ok(carsViewModel);
+
+                //exemplo com dapper
+                //using (var sqlConection = new SqlConnection(_connectionString))
+                //{
+                //    var query = "SELECT id, Brand, Model, Price FROM Cars WHERE Status = 0";
+
+                //    var carsViewModel = sqlConection.Query<CarItemViewModel>(query);
+                //    return Ok(carsViewModel);
+                //}
+                
             }
             catch (Exception ex)
             {
@@ -104,6 +140,16 @@ namespace DevCars.API.Controllers
 
                 car.Update(model.Color, model.Price);
                 _dbContext.SaveChanges();
+                
+                //com dapper
+                //using (var sqlConection = new SqlConnection(_connectionString))
+                //{
+                //    var query = "UPDATE Cars SET Color = @color, Price = @price WHERE Id = @id";
+
+                //    sqlConection.Execute(query, new { color = car.Color, price = car.Price, car.Id });
+
+                //}
+
                 return NoContent();
             }
             catch (Exception ex )
